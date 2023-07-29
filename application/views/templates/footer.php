5240d85047360c128976200fc0c33fac5f18a4c3
SARED\kitog <a20200196@utem.edu.mx>
2023-07-28 21:12:56 -0600

    Footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -44,61 +44,61 @@
                             </div>
                             <div class="col-sm-6">
                                 <div class="text-sm-end d-none d-sm-block">
-                                    
+
                                 </div>
                             </div>
-                        </div>
                     </div>
-                </footer>
+                </div>
+</footer>
+</div>
+<!-- end main content-->
+
+</div>
+<!-- END layout-wrapper -->
+
+</div>
+<!-- end container-fluid -->
+
+<!-- Modal para mostrar resultados -->
+<div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Resultados</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <!-- end main content-->
-
-        </div>
-        <!-- END layout-wrapper -->
-
-    </div>
-    <!-- end container-fluid -->
-
-    <!-- Modal para mostrar resultados -->
-    <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Resultados</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <!-- Tabla para mostrar los resultados -->
-                    <table class="table mb-0">
-                        <thead>
-                            <tr>
-                                <th>Examen</th>
-                                <th>Porcentaje Obtenido</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <!-- Filas de ejemplo de resultados -->
-                            <tr>
-                                <td>Examen 1</td>
-                                <td>85%</td>
-                            </tr>
-                            <tr>
-                                <td>Examen 2</td>
-                                <td>92%</td>
-                            </tr>
-                            <tr>
-                                <td>Examen 3</td>
-                                <td>78%</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                </div>
+            <div class="modal-body">
+                <!-- Tabla para mostrar los resultados -->
+                <table class="table mb-0">
+                    <thead>
+                        <tr>
+                            <th>Examen</th>
+                            <th>Porcentaje Obtenido</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- Filas de ejemplo de resultados -->
+                        <tr>
+                            <td>Examen 1</td>
+                            <td>85%</td>
+                        </tr>
+                        <tr>
+                            <td>Examen 2</td>
+                            <td>92%</td>
+                        </tr>
+                        <tr>
+                            <td>Examen 3</td>
+                            <td>78%</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
+</div>
 </footer>
 
 
@@ -122,24 +122,24 @@
 <script src="<?php echo base_url(); ?>assets/libs/jquery-sparkline/jquery.sparkline.min.js"></script>
 
 
-    <script src="<?php echo base_url(); ?>assets/libs/select2/js/select2.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/libs/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/libs/bootstrap-maxlength/bootstrap-maxlength.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/libs/jquery.repeater/jquery.repeater.min.js"></script>
-              
-    <!-- form advanced init -->
-    <script src="<?php echo base_url(); ?>assets/js/pages/form-advanced.init.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/pages/form-repeater.init.js"></script> 
+<script src="<?php echo base_url(); ?>assets/libs/select2/js/select2.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/libs/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/libs/bootstrap-maxlength/bootstrap-maxlength.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/libs/jquery.repeater/jquery.repeater.min.js"></script>
 
-    <!-- Plugins js -->
-    <script src="<?php echo base_url(); ?>assets/libs/dropzone/min/dropzone.min.js"></script>
+<!-- form advanced init -->
+<script src="<?php echo base_url(); ?>assets/js/pages/form-advanced.init.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/pages/form-repeater.init.js"></script>
 
-    <!-- App js -->
-    <script src="<?php echo base_url(); ?>assets/js/app.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/scrips.js"></script>
+<!-- Plugins js -->
+<script src="<?php echo base_url(); ?>assets/libs/dropzone/min/dropzone.min.js"></script>
 
-    <script src="<?php echo base_url(); ?>assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+<!-- App js -->
+<script src="<?php echo base_url(); ?>assets/js/app.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/scrips.js"></script>
+
+<script src="<?php echo base_url(); ?>assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
 <!-- Buttons examples -->
 <script src="<?php echo base_url(); ?>assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
@@ -152,7 +152,8 @@
 <script src="<?php echo base_url(); ?>assets/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
 <!-- Responsive examples -->
 <script src="<?php echo base_url(); ?>assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+<script
+    src="<?php echo base_url(); ?>assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
 
 <!-- Datatable init js -->
 <script src="<?php echo base_url(); ?>assets/js/pages/datatables.init.js"></script>
