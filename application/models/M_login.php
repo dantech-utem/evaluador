6a2008c54e5ad79429a550f2079478043f484ddf
SARED\kitog <a20200196@utem.edu.mx>
2023-07-28 19:55:30 -0600

    Cambios Login

--- application/models/M_login.php
+++ application/models/M_login.php
@@ -4,18 +4,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class M_login extends CI_Model {
 //LOGIN
 
-    public function obtenerUsuario($correo, $contraseña) {
-        $this->db->where('nombre', $correo);
-        $this->db->where('contraseña', $contraseña);
-        
-        $query = $this->db->get('usuarios');
+public function obtenerUsuario($correo, $contraseña) {
+    $this->db->where('nombre', $correo);
+    $this->db->where('contraseña', $contraseña);
+    
+    // Agregar el filtro del estado del usuario (asumiendo que el campo se llama 'estado')
+    $this->db->where('estatus_usuario', 1);
 
-        if ($query->num_rows() == 1) {
-            return $query->row();
-        } else {
-            return false;
-        }
+    $query = $this->db->get('usuarios');
+
+    if ($query->num_rows() == 1) {
+        return $query->row();
+    } else {
+        return false;
     }
+}
+
 //RECUPER CONTRASEÑA
 
     public function obtenerCorreo($correo) {
